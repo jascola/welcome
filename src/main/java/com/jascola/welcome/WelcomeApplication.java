60dd1ab0a0f7c05bc731c8a5259d9e98b752ba46
jascola <1947857236@qq.com>
2020-06-03 21:13:43 +0800

    test docker redis-template

--- src/main/java/com/jascola/welcome/WelcomeApplication.java
+++ src/main/java/com/jascola/welcome/WelcomeApplication.java
@@ -2,6 +2,9 @@ package com.jascola.welcome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class WelcomeApplication {
@@ -9,5 +12,10 @@ public class WelcomeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WelcomeApplication.class, args);
 	}
-
+	@Bean
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, String> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+		return template;
+	}
 }
--- src/main/java/com/jascola/welcome/web/controller/TestController.java
+++ src/main/java/com/jascola/welcome/web/controller/TestController.java
@@ -3,23 +3,31 @@ package com.jascola.welcome.web.controller;
 import com.github.pagehelper.PageInfo;
 import com.jascola.welcome.web.dao.TestDao;
 import com.jascola.welcome.web.entity.TestEntity;
+import org.apache.commons.lang.StringUtils;
 import org.apache.ibatis.session.RowBounds;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class TestController {
 
+    private final RedisTemplate<String,String> redisTemplate;
     private final TestDao testDao;
     private Logger logger = LogManager.getLogger("com.jascola.welcome.web.controller.TestController");
-
-    public TestController(TestDao testDao) {
+    public TestController(TestDao testDao, RedisTemplate<String, String> redisTemplate) {
         this.testDao = testDao;
+        this.redisTemplate = redisTemplate;
     }
 
     @RequestMapping("/index")
@@ -36,5 +44,27 @@ public class TestController {
         logger.info(new PageInfo<>(testDao.getTestEntity(testEntity)));
         logger.info(testDao.getTestEntityRow(new RowBounds(1,1)));
         return modelAndView;
+    }
+
+    @RequestMapping("/redis")
+    @ResponseBody
+    public String redis(){
+        TestEntity testEntity = new TestEntity();
+        testEntity.setMyMoney(Money.of(CurrencyUnit.of("CNY"), 100000000L));
+        testEntity.setUserName("jascola");
+        testEntity.setPageParam(1,1);
+        //testDao.insert(testEntity);
+        List<TestEntity> list = testDao.getTestEntity(testEntity);
+        ValueOperations<String,String> valueOperations = redisTemplate.opsForValue();
+        String value = valueOperations.get("test-ket");
+        if(StringUtils.isNotBlank(value)){
+            redisTemplate.delete("test-ket");
+            logger.info("test-ket");
+            return value;
+        }else{
+            valueOperations.set("test-ket","jsonString");
+            redisTemplate.expire("test-ket",1, TimeUnit.MINUTES);
+            return "jsonString";
+        }
     }
 }
